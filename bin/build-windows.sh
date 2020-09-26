c24e8333f0a46f90c307d6e3dd1f51bef1446fa0
Bryan <WolfkillArcadia@gmail.com>
2020-09-26 11:51:51 -0400

    Build scripts

--- bin/build-windows.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the 32-bit DLL
-rustup run stable-i686-pc-windows-msvc cargo build --target i686-pc-windows-msvc
-cp ./target/i686-pc-windows-msvc/esm.dll ./esm.dll
-
-# Build the 64-bit DLL
-rustup run stable-x86_64-pc-windows-msvc cargo build --target x86_64-pc-windows-msvc
-cp ./target/x86_64-pc-windows-msvc/esm.dll ./esm_x64.dll
