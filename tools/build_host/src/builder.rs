eeb55b5e3ef114c755fa6ff1093d4cb926135001
WolfkillArcadia <WolfkillArcadia@gmail.com>
2022-05-30 18:03:08 -0400

    Added initial network setup and systemcommand

--- tools/build_host/src/builder.rs
+++ tools/build_host/src/builder.rs
@@ -1,22 +1,29 @@
-use super::{BuildOS, BuildEnv, LogLevel};
+use super::{BuildOS, BuildEnv, LogLevel, BuildArch};
 
-#[derive(Debug)]
-pub enum BuildArch {
-    X32,
-    X64,
-}
+use message_io::network::{NetEvent, Transport, Endpoint};
+use message_io::node::{self, NodeTask, NodeHandler};
+use serde::{Serialize, Deserialize};
+use colored::*;
+use lazy_static::lazy_static;
+
+use std::collections::{HashMap};
+use std::fs::{File};
 
 pub struct Builder {
-    pub target: BuildOS,
-    pub arch: BuildArch,
-    pub env: BuildEnv,
-    pub log_level: LogLevel,
-    pub git_directory: String,
-    pub build_directory: String,
+    os: BuildOS,
+    arch: BuildArch,
+    env: BuildEnv,
+    log_level: LogLevel,
+    git_directory: String,
+    build_directory: String,
+
+    server_task: Option<NodeTask>,
+    handler: Option<NodeHandler<()>>,
+    endpoint: Option<Endpoint>,
 }
 
 impl Builder {
-    pub fn new(build_x32: bool, target: BuildOS,  log_level: LogLevel, env: BuildEnv) -> Self {
+    pub fn new(build_x32: bool, os: BuildOS,  log_level: LogLevel, env: BuildEnv) -> Self {
         let git_directory = match std::env::current_dir() {
             Ok(d) => d.to_string_lossy().to_string(),
             Err(e) => panic!("{e}")
@@ -25,12 +32,104 @@ impl Builder {
         let build_directory = format!("{}/target/@esm", git_directory);
 
         Builder {
-            target,
+            os,
             arch: if build_x32 { BuildArch::X32 } else { BuildArch::X64 },
             env,
             log_level,
             git_directory,
             build_directory,
+            server_task: None,
+            handler: None
         }
     }
+
+    fn print_status<F>(&mut self, message: impl Into<String> + std::fmt::Display, code: F)
+    where
+        F: Fn(&mut Builder)
+    {
+        print!("{} - {message} ... ", "<esm_bt>".blue().bold());
+        code(self);
+        println!("{}", "done".green().bold());
+    }
+
+    pub fn start(&mut self) {
+        self.print_info();
+        self.print_status("Starting build server", Builder::start_server);
+        self.print_status("Killing Arma", Builder::kill_arma);
+    }
+
+    fn print_info(&self) {
+        println!(
+            "{}\n  {:17}: {:?}\n  {:17}: {:?}\n  {:17}: {:?}\n  {:17}: {:?}\n  {:17}: {}\n  {:17}: {}\n",
+            "ESM Build tool".blue().bold(),
+            "OS".black().bold(), self.os,
+            "ARCH".black().bold(), self.arch,
+            "ENV".black().bold(), self.env,
+            "LOG LEVEL".black().bold(), self.log_level,
+            "GIT DIRECTORY".black().bold(), self.git_directory,
+            "BUILD DIRECTORY".black().bold(), self.build_directory
+        )
+    }
+
+    fn start_server(&mut self) {
+        let (handler, listener) = node::split::<()>();
+
+        let listen_addr = "0.0.0.0:6969";
+        handler.network().listen(Transport::FramedTcp, listen_addr).unwrap();
+        let mut transfers: HashMap<Endpoint, Transfer> = HashMap::new();
+
+        let task = listener.for_each_async(move |event| match event.network() {
+            NetEvent::Connected(_, _) => unreachable!(),
+            NetEvent::Accepted(_, _) => (),
+            NetEvent::Message(endpoint, input_data) => {
+                let message: NetworkCommands = bincode::deserialize(input_data).unwrap();
+                match message {
+                    _ => ()
+                }
+            }
+            NetEvent::Disconnected(endpoint) => {}
+        });
+
+        self.server_task = Some(task);
+        self.handler = Some(handler);
+    }
+
+    fn send_to_receiver(&self, command: NetworkCommands) {
+        let data = bincode::serialize(&command).unwrap();
+        self.handler.unwrap().network().send(self.endpoint.unwrap(), &data);
+    }
+
+    fn kill_arma(&mut self) {
+        lazy_static! {
+            static ref WINDOWS_EXES: Vec<&'static str> = vec![
+                "arma3server.exe", "arma3server_x64.exe",
+                "arma3_x64.exe", "arma3.exe", "arma3battleye.exe"
+            ];
+
+            static ref LINUX_EXES: Vec<&'static str> = vec!["arma3server", "arma3server_x64"];
+        };
+
+        match self.os {
+            BuildOS::Windows => {
+                for exe in WINDOWS_EXES.into_iter() {
+                    let command = format!("taskkill /IM \"{}\" /F /T >nul 2>&1", exe);
+                    self.send_to_receiver(NetworkCommands::SystemCommand(command));
+                }
+            },
+            BuildOS::Linux => todo!(),
+        }
+    }
+
+}
+
+struct Transfer {
+    file: File,
+    name: String,
+    current_size: usize,
+    expected_size: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+enum NetworkCommands {
+    SystemCommand(String)
 }
--- tools/build_host/src/main.rs
+++ tools/build_host/src/main.rs
@@ -4,12 +4,6 @@
 mod builder;
 
 use builder::Builder;
-use message_io::network::{NetEvent, Transport};
-use message_io::node::{self, NodeEvent};
-use std::fs::{self, File};
-use std::io::{Read};
-use std::time::{Duration};
-use serde::{Serialize, Deserialize};
 use clap::{Parser, ArgEnum, Subcommand};
 
 /// Builds ESM's Arma 3 server mod
@@ -62,120 +56,20 @@ pub enum BuildEnv {
     Production
 }
 
-#[derive(Serialize, Deserialize)]
-pub enum SenderMsg {
-    //From sender to receiver
-    FileRequest(String, usize), // name, size
-    Chunk(Vec<u8>),             // data
+#[derive(Debug)]
+pub enum BuildArch {
+    X32,
+    X64,
 }
-
-#[derive(Serialize, Deserialize)]
-pub enum ReceiverMsg {
-    //From receiver to sender
-    CanReceive(bool),
-}
-
-enum Signal {
-    SendChunk,
-    // Other signals here
-}
-
-const CHUNK_SIZE: usize = 65536;
 
 fn main() {
     let args = Args::parse();
 
-    let builder = match args.command {
+    let mut builder = match args.command {
         Commands::Run { build_x32, target, log_level, env } => {
             Builder::new(build_x32, target, log_level, env)
         }
     };
 
-    print_info(&builder);
-
-    // Spin up the server
-    // Kill arma
-}
-
-
-pub fn run(file_path: String) {
-    let (handler, listener) = node::split();
-
-    let server_addr = "127.0.0.1:3005";
-    let (server_id, _) = handler.network().connect(Transport::FramedTcp, server_addr).unwrap();
-
-    let file_size = fs::metadata(&file_path).unwrap().len() as usize;
-    let mut file = File::open(&file_path).unwrap();
-    let file_name: String = file_path.rsplit('/').into_iter().next().unwrap_or(&file_path).into();
-
-    let mut file_bytes_sent = 0;
-    listener.for_each(move |event| match event {
-        NodeEvent::Network(net_event) => match net_event {
-            NetEvent::Connected(_, established) => {
-                if established {
-                    println!("Sender connected by TCP at {}", server_addr);
-                    let request = SenderMsg::FileRequest(file_name.clone(), file_size);
-                    let output_data = bincode::serialize(&request).unwrap();
-                    handler.network().send(server_id, &output_data);
-                }
-                else {
-                    println!("Can not connect to the receiver by TCP to {}", server_addr)
-                }
-            }
-            NetEvent::Accepted(_, _) => unreachable!(),
-            NetEvent::Message(_, input_data) => {
-                let message: ReceiverMsg = bincode::deserialize(&input_data).unwrap();
-                match message {
-                    ReceiverMsg::CanReceive(can) => match can {
-                        true => handler.signals().send(Signal::SendChunk), // Start sending
-                        false => {
-                            handler.stop();
-                            println!("The receiver can not receive the file :(");
-                        }
-                    },
-                }
-            }
-            NetEvent::Disconnected(_) => {
-                handler.stop();
-                println!("\nReceiver disconnected");
-            }
-        },
-        NodeEvent::Signal(signal) => match signal {
-            Signal::SendChunk => {
-                let mut data = [0; CHUNK_SIZE];
-                let bytes_read = file.read(&mut data).unwrap();
-                if bytes_read > 0 {
-                    let chunk = SenderMsg::Chunk(Vec::from(&data[0..bytes_read]));
-                    let output_data = bincode::serialize(&chunk).unwrap();
-                    handler.network().send(server_id, &output_data);
-                    file_bytes_sent += bytes_read;
-
-                    let percentage = ((file_bytes_sent as f32 / file_size as f32) * 100.0) as usize;
-                    print!("\rSending '{}': {}%", file_name, percentage);
-
-                    handler.signals().send_with_timer(Signal::SendChunk, Duration::from_micros(10));
-                }
-                else {
-                    println!("\nFile sent!");
-                    handler.stop();
-                }
-            }
-        },
-    });
-}
-
-
-fn print_info(builder: &Builder) {
-    println!(
-r#"| ESM Build tool
-|   OS: {:?}
-|   ARCH: {:?}
-|   ENV: {:?}
-|   LOG_LEVEL: {:?}
-|   GIT_DIRECTORY: {}
-|   BUILD_DIRECTORY: {}
-"#,
-        builder.target, builder.arch, builder.env,
-        builder.log_level, builder.git_directory, builder.build_directory
-    )
+    builder.start();
 }
