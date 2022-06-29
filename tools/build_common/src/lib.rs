79d88e1d2b9c714b7f40fe5dc11cc104d4059d3c
WolfkillArcadia <WolfkillArcadia@gmail.com>
2022-06-29 15:51:15 -0400

    Rewrote how messages are sent over the network Added helper methods

--- tools/build_common/src/lib.rs
+++ tools/build_common/src/lib.rs
@@ -1,21 +1,45 @@
+use std::time::Duration;
+
+use parking_lot::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 pub mod error;
 pub use error::*;
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum NetworkCommands {
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct NetworkCommand {
+    pub id: Uuid,
+    pub command: Command,
+}
+
+impl NetworkCommand {
+    pub fn new(command: Command) -> Self {
+        NetworkCommand {
+            id: Uuid::new_v4(),
+            command,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Command {
     Hello,
     Success,
     Error(String),
-    SystemCommand(String, Vec<String>),
+    System(String, Vec<String>),
     FileTransferStart(FileTransfer),
     FileTransferChunk(FileChunk),
     FileTransferEnd(Uuid),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+impl Default for Command {
+    fn default() -> Self {
+        Command::Hello
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct FileTransfer {
     pub id: Uuid,
     pub file_name: String,
@@ -24,7 +48,7 @@ pub struct FileTransfer {
     pub total_size: usize,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct FileChunk {
     pub id: Uuid,
     pub index: usize,
@@ -33,3 +57,45 @@ pub struct FileChunk {
 }
 
 pub type BuildResult = Result<(), BuildError>;
+
+pub fn read_lock<T, F>(lock: &RwLock<T>, wait: Duration, code: F)
+where
+    F: Fn(RwLockReadGuard<T>) -> bool,
+{
+    loop {
+        let reader = match lock.try_read() {
+            Some(r) => r,
+            None => {
+                std::thread::sleep(wait);
+                continue;
+            }
+        };
+
+        if code(reader) {
+            break;
+        }
+
+        std::thread::sleep(wait);
+    }
+}
+
+pub fn write_lock<T, F>(lock: &RwLock<T>, wait: Duration, code: F)
+where
+    F: Fn(RwLockWriteGuard<T>) -> bool,
+{
+    loop {
+        let writer = match lock.try_write() {
+            Some(w) => w,
+            None => {
+                std::thread::sleep(wait);
+                continue;
+            }
+        };
+
+        if code(writer) {
+            break;
+        }
+
+        std::thread::sleep(wait);
+    }
+}
