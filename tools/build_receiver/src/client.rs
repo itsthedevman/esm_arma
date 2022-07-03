8fc3728735b94ccc33dbe39f98a7710696564431
Bryan <WolfkillArcadia@gmail.com>
2022-07-02 22:13:15 -0400

    Implemented changes for System command

--- tools/build_receiver/src/client.rs
+++ tools/build_receiver/src/client.rs
@@ -3,11 +3,12 @@ use std::process::Command as SystemCommand;
 use std::sync::Arc;
 use std::time::Duration;
 
-use crate::{read_lock, transfer::*, write_lock, BuildError, BuildResult, Command, NetworkCommand};
+use crate::{read_lock, transfer::*, write_lock, BuildResult, Command, NetworkCommand, System};
 use colored::Colorize;
 use message_io::network::{Endpoint, NetEvent, Transport};
 use message_io::node::{self, NodeHandler, NodeTask};
 use parking_lot::RwLock;
+use regex::Regex;
 
 #[derive(Clone)]
 pub struct Client {
@@ -119,7 +120,7 @@ pub struct IncomingCommand;
 impl IncomingCommand {
     pub fn execute(client: &Client, network_command: &Command) -> BuildResult {
         match network_command {
-            Command::System(command, args) => IncomingCommand.system_command(command, args),
+            Command::System(command) => IncomingCommand.system_command(command),
             Command::FileTransferStart(transfer) => read_lock(
                 &client.transfers,
                 Duration::from_secs_f32(0.1),
@@ -148,20 +149,36 @@ impl IncomingCommand {
         }
     }
 
-    fn system_command(&self, command: &str, args: &[String]) -> BuildResult {
-        let result = SystemCommand::new(command).args(args).output();
+    fn system_command(&self, command: &System) -> BuildResult {
+        let result = SystemCommand::new(&command.cmd)
+            .args(&command.args)
+            .output();
 
-        println!("Command result: {:#?}", result);
         match result {
-            Ok(_output) => {
-                // println!("  Status: {}", output.status);
-                // println!("  OUT: {}", String::from_utf8_lossy(&output.stdout));
-                // println!("  ERR: {}", String::from_utf8_lossy(&output.stderr));
+            Ok(output) => {
+                if command.check_for_success {
+                    let status = output.status;
+                    let stdout = String::from_utf8_lossy(&output.stdout);
+                    let stderr = String::from_utf8_lossy(&output.stderr);
+
+                    if !status.success() {
+                        return Err(stderr.to_string().into());
+                    }
+
+                    // let regex = match Regex::from_str(&command.success_regex) {
+                    //     Ok(r) => r,
+                    //     Err(e) => return Err(e.to_string().into()),
+                    // };
+
+                    println!("STDOUT: {}", stdout);
+                    println!("STDERR: {}", stderr);
+                }
+
                 Ok(())
             }
             Err(e) => {
                 println!("{}", format!("Failed! {e}").red());
-                Err(BuildError::from(e))
+                Err(e.to_string().into())
             }
         }
     }
