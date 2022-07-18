82dfc9f109283e3df7b5bff367be569a2e733f82
Bryan <WolfkillArcadia@gmail.com>
2022-07-18 19:23:41 -0400

    Added mysql access to receiver

--- bin/build_receiver.bat
+++ bin/build_receiver.bat
@@ -1,4 +1,4 @@
 @echo off
 
 cd build/receiver
-cargo run --release -- --host esm.mshome.net:54321
+cargo run --release -- --host esm.mshome.net:54321 --database-uri mysql://root:password12345@localhost:3306/exile_test_esm
