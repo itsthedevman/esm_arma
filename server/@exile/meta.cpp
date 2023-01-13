29543193eebb30c45ffc28e0c66c66ed9ef08e87
Bryan <8569851+itsthedevman@users.noreply.github.com>
2023-01-12 19:46:08 -0500

    Added server files for a3 linux

---
+++ server/@exile/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1487484880;
+name = "ExileMod";
+timestamp = 5248705906270557284;
---
+++ server/@exile/mod.cpp
@@ -0,0 +1,10 @@
+name = "Exile Mod";
+author = "Exile Mod Team";
+action = "http://www.exilemod.com";
+dir = "@Exile";
+logo = "mod.paa";
+logoOver = "mod.paa";
+picture = "exile_assets\texture\mod\icon.paa";
+hidePicture = 0;
+hideName = 0;
+overview = "";
