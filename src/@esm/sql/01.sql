199f149b5e620956f0f45c6c87b90420dc0830ce
Bryan <bryan@itsthedevman.com>
2024-10-17 19:08:09 -0400

    Updated xm8 notification table

--- src/@esm/sql/01.sql
+++ src/@esm/sql/01.sql
@@ -10,10 +10,13 @@ ADD COLUMN `esm_payment_counter` INT(11) UNSIGNED NOT NULL DEFAULT '0' AFTER `mo
 -- Adds `xm8_notification` table. This table stores all outbound XM8 notifications
 CREATE TABLE xm8_notification (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(36) NOT NULL,
     recipient_uid VARCHAR(32) NOT NULL,
-    territory_id INT,
+    territory_id INT UNSIGNED,
     type VARCHAR(50) NOT NULL,
     content TEXT NOT NULL,
+    -- new, sent, fail (with reason)
+    status VARCHAR(50) NOT NULL DEFAULT 'new',
     attempt_count INT DEFAULT 0,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_attempt_at DATETIME,
@@ -21,12 +24,6 @@ CREATE TABLE xm8_notification (
     INDEX idx_recipient_uid (recipient_uid),
     INDEX idx_territory_id (territory_id),
     INDEX idx_last_attempt_acknowledged (last_attempt_at, acknowledged_at, attempt_count),
-    CONSTRAINT fk_recipient_account FOREIGN KEY (recipient_uid)
-        REFERENCES account (uid)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    CONSTRAINT fk_territory FOREIGN KEY (territory_id)
-        REFERENCES territory (id)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
+    CONSTRAINT fk_recipient_account FOREIGN KEY (recipient_uid) REFERENCES account (uid) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_territory FOREIGN KEY (territory_id) REFERENCES territory (id) ON DELETE SET NULL ON UPDATE CASCADE
 ) DEFAULT CHARSET = utf8mb4;
