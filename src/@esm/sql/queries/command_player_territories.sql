06ca858b9e1812174fe8f713cc593ea03fdec873
Bryan <bryan@itsthedevman.com>
2024-11-28 14:55:06 -0500

    Standardization

--- src/@esm/sql/queries/command_player_territories.sql
+++ src/@esm/sql/queries/command_player_territories.sql
@@ -1,31 +1,27 @@
 SELECT
-    territory.id,
-    territory.owner_uid,
-    account.name AS owner_name,
-    territory.name AS territory_name,
-    territory.radius,
-    territory.level,
-    territory.flag_texture,
-    territory.flag_stolen,
-    CONVERT_TZ(
-        territory.last_paid_at,
-        @@GLOBAL.time_zone,
-        '+00:00'
-    ) AS last_paid_at,
-    territory.build_rights,
-    territory.moderators,
-    COUNT(construction.id) AS object_count,
-    territory.esm_custom_id
+    t.id,
+    t.owner_uid,
+    a.name as owner_name,
+    t.name as territory_name,
+    t.radius,
+    t.level,
+    t.flag_texture,
+    t.flag_stolen,
+    CONVERT_TZ(t.last_paid_at, @@GLOBAL.time_zone, '+00:00') as last_paid_at,
+    t.build_rights,
+    t.moderators,
+    COUNT(c.id) as object_count,
+    t.esm_custom_id
 FROM
-    territory
-    LEFT JOIN account ON territory.owner_uid = account.uid
-    LEFT JOIN construction ON territory.id = construction.territory_id
+    territory t
+    LEFT JOIN account a ON t.owner_uid = a.uid
+    LEFT JOIN construction c ON t.id = c.territory_id
 WHERE
-    territory.deleted_at IS NULL
+    t.deleted_at IS NULL
     AND (
-        territory.owner_uid = :player_uid
-        OR territory.build_rights LIKE :wildcard_uid
-        OR territory.moderators LIKE :wildcard_uid
+        t.owner_uid = :player_uid
+        OR t.build_rights LIKE :wildcard_uid
+        OR t.moderators LIKE :wildcard_uid
     )
 GROUP BY
-    territory.id
+    t.id
