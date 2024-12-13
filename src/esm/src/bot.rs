d2bd291292207b013f920fab0147e5f450a8b750
Bryan <bryan@itsthedevman.com>
2024-12-12 22:42:30 -0500

    Add length header on send

--- src/esm/src/bot.rs
+++ src/esm/src/bot.rs
@@ -253,7 +253,20 @@ fn send_request(request: Request) -> ESMResult {
     };
 
     // Encode
-    let request = encryption::BASE64_STANDARD.encode(request).into_bytes();
+    let mut request = encryption::BASE64_STANDARD.encode(request).into_bytes();
+
+    // Add the length header
+    let length = (request.len() as u32).to_be_bytes();
+
+    info!(
+        "DATA | length: {}, size: {}, content: {:?}",
+        u32::from_be_bytes(length),
+        request.len(),
+        String::from_utf8_lossy(&request)
+    );
+
+    // Add the header
+    request.splice(0..0, length);
 
     // Send
     match lock!(HANDLER)
